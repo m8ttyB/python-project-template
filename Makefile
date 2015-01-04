f4add75b7cf27b0d29b7c1846685eb46ccbccd4a
Richard Pappalardo <rpappalax@gmail.com>
2015-01-04 00:39:17 -0800

    Adding convenience options to Makefile for pypi.

--- Makefile
+++ Makefile
@@ -23,3 +23,29 @@ clean:
 clobber: clean
 	rm -rf $(VENVDIR)
 	rm -rf *egg*
+	rm -rf dist
+
+# dev branch only - below this line
+
+.PHONY: pypi
+pypi:
+	# Upload the package to PyPi
+	python setup.py sdist upload -r pypi
+	python setup.py bdist_egg upload -r pypi
+
+.PHONY: testpypi
+testpypi:
+	# Create a dist dir
+	# Upload the package to PyPiTest
+	python setup.py sdist upload -r pypitest
+	# creating 'dist/dummy_project-0.0.3-py2.7.egg' and
+	#  adding 'build/bdist.macosx-10.9-intel/egg' to it
+	python setup.py bdist_egg upload -r pypitest
+
+.PHONY: pypi-register
+pypi-register:
+	python setup.py register -r pypi
+
+.PHONY: testpypi-register
+testpypi-register:
+	python setup.py register -r testpypi
--- setup.py
+++ setup.py
@@ -4,26 +4,27 @@ from setuptools import setup
 
 setup(
     name = 'dummy-project',
-    version = '0.0.1',
+    version = '0.0.3',
     description = 'Python script that does absolutely nothing except \
     to demonstrate setup.py.',
     author = 'Richard Pappalardo',
     author_email = 'rpappalax@gmail.com',
     url = 'https://github.com/rpappalax/dummy-project',
     install_requires = [ 'Fabric' ],
+    keywords = ['dummy', 'setup.py', 'project'],
     packages = [ 'dummyproj' ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
     entry_points={
         'console_scripts': [
-            'dummy = dummyproj.dummy_handler:DummyHandler',
+            'dummyproj = dummyproj.dummy_handler:DummyHandler',
         ]
     },
-    keywords = ['dummy', 'setup.py', 'project'],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-    ],
+
 )
