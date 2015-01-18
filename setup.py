de630f3d598b7ed202cdc6d8b79ea30a5160c87f
Richard Pappalardo <rpappalax@gmail.com>
2015-01-17 23:44:13 -0800

    Update README with reference links

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'python-project-template',
-    version = '0.0.6',
+    version = '0.0.7',
     description = 'Python project template to demonstrate setup.py,'
                   'with Makefile build and sample unittests.',
     author = 'Richard Pappalardo',
