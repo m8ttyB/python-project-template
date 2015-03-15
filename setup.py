cd7051d30e475659aa0af618c3acade09f595978
Richard Pappalardo <rpappalax@gmail.com>
2015-03-14 20:02:20 -0700

    cleanup config files.

--- setup.py
+++ setup.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+# from setuptools import setup
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+REQUIREMENTS = [
+    'nose >= 1.3.4',
+    'tox >= 1.9.0'
+]
+
+KEYWORDS = [
+    'demo',
+    'setup.py',
+    'project'
+]
 
 setup(
     name='python-project-template',
-    version='0.0.8',
-    description='Python project template to demonstrate setup.py \
-                with Makefile build and sample unittests.',
+    version='0.0.9',
+    description='python-project-template',
+    long_description=README,
     author='Richard Pappalardo',
     author_email='rpappalax@gmail.com',
     url='https://github.com/rpappalax/python-project-template',
     license="MIT",
-    install_requires=['nose >= 1.3.4', 'tox >= 1.9.0'],
-    keywords=['demo', 'setup.py', 'project'],
-    packages=['demo'],
+    install_requires=REQUIREMENTS,
+    keywords=KEYWORDS,
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
     ],
     entry_points={
