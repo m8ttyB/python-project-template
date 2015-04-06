e98a175004e47e0bca78d587b4d88240b9427f1d
Richard Pappalardo <rpappalax@gmail.com>
2015-04-05 18:02:17 -0700

    travis test

--- tests/test_dummy_handler.py
+++ tests/test_dummy_handler.py
@@ -13,7 +13,8 @@ class DemoTestCase(unittest.TestCase):
     def test_some_bool(self):
         """Does this function actually return True?"""
         print('access_token=XXXXX')
-        self.assertTrue(d.some_bool())
+        # self.assertTrue(d.some_bool())
+        self.assertFalse(d.some_bool())
 
     def test_do_nothing(self):
         """Does this function actually return False?"""
